74d8dd7aadcf5811e6a540173476b3e47e48868b
Nate <natewong1@GMAIL.COM>
2024-02-20 20:58:39 -0500

    Add cache & readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# pitt-internship-api
+
+Rest api that scrapes the [Summer 2024 Tech Internships by Pitt CSC & Simplify](https://github.com/SimplifyJobs/Summer2024-Internships) page for internships
--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
-	"github.com/natewong1313/pitt-intern-api/scraper"
+	"github.com/natewong1313/pitt-internship-api/scraper"
+	gocache "github.com/patrickmn/go-cache"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	// Cache the job listings for 1 hour
+	cache := gocache.New(1*time.Hour, 10*time.Minute)
+	cache.Set("listings", scraper.Scrape(), gocache.DefaultExpiration)
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		var listings []scraper.JobListing
+		cachedListings, found := cache.Get("listings")
+		if !found {
+			listings = scraper.Scrape()
+			cache.Set("listings", listings, gocache.DefaultExpiration)
+		} else {
+			listings = cachedListings.([]scraper.JobListing)
+		}
 		c.JSON(200, gin.H{
-			"listings": scraper.Scrape(),
+			"listings": listings,
 		})
 	})
-	r.Run()
+	router.Run()
 }
